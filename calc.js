b9ecb75d41f00432ceb827530d693bb148e94a3a
anumathi <97147953+anumathi@users.noreply.github.com>
2022-01-04 23:04:41 -0800

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,16 @@
+let outscreen=document.getElementById("out-screen");
+
+function display(num) {
+
+	outscreen.value+=num;
+	// body...
+}
+/*function calculate {
+	try{
+		out-screen.value=equal(out-screen.value);
+	}
+	catch{
+		alert("Invalid");
+	}
+	// body...
+}
